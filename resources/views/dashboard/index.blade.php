25039c08232575622f7531c4fed2e6a08020613c
Clement Goustiaux <clement_goustiaux@ens.univ-artois.fr>
2024-12-16 13:36:07 +0100

    🗿 Vue Dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,1 +1,18 @@
-<?php
+@extends('layouts.layout')
+
+@section('title', 'Tableau de bord')
+
+@section('content')
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header">
+                <h3>Tableau de bord</h3>
+            </div>
+            <div class="card-body">
+                <p><strong>Nom :</strong> {{ $user->name }}</p>
+                <p><strong>Email :</strong> {{ $user->email }}</p>
+                <p><strong>Date de création :</strong> {{ $user->created_at->format('d/m/Y') }}</p>
+            </div>
+        </div>
+    </div>
+@endsection
