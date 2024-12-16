7d57969d3c9fb4cfddbe931293c4a2c2fbde0197
Delaude Theo <theo_delaude@ens.univ-artois.fr>
2024-12-16 16:21:06 +0100

    #10 modifification de CSS

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <header class="bg-dark text-white py-2 fixed-top w-100 z-index-1000">
-    <nav>
+    <nav class="d-flex justify-content-center align-items-center">
         <ul class="list-unstyled d-flex justify-content-center mb-0">
             <li class="mx-3"><a href="{{ route('home') }}" class="text-white text-decoration-none p-2 rounded hover-bg-dark">Accueil</a></li>
             <li class="mx-3"><a href="{{ route('about') }}" class="text-white text-decoration-none p-2 rounded hover-bg-dark">À propos</a></li>
@@ -7,14 +7,16 @@
             @auth
                 <li class="mx-3"><a href="{{ route('dashboard') }}" class="text-white text-decoration-none p-2 rounded hover-bg-dark">Dashboard</a></li>
                 <li class="mx-3">
-                    <form method="POST" action="{{ route('logout') }}" class="d-inline">
+                    <form method="POST" action="{{ route('logout') }}" class="bg-transparent border-0 p-0 shadow-none align-self-center">
                         @csrf
-                        <button type="submit" class="btn btn-danger p-2">Déconnexion</button>
+                        <button type="submit" class="btn btn-danger p-2 border-0 shadow-none">Déconnexion</button>
                     </form>
                 </li>
             @else
                 <li class="mx-3"><a href="{{ route('login') }}" class="text-white text-decoration-none p-2 rounded hover-bg-dark">Connexion</a></li>
-                <li class="mx-3"><a href="{{ route('register') }}" class="text-white text-decoration-none p-2 rounded hover-bg-dark">Inscription</a></li>
+                <li class="mx-3">
+                    <a href="{{ route('register') }}" class="btn btn-danger p-2 border-0 shadow-none">Inscription</a>
+                </li>
             @endauth
         </ul>
     </nav>
